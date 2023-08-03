750133900fc67110a76754870aeac8d254d3839d
GNUAaryn <90221167+GNUAaryn@users.noreply.github.com>
2023-08-03 03:59:50 -0500

    Update config.h
    using alacritty term

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *bruh[]  = { "firefox", NULL };
 static const char *pic[]  = { "flameshot", "gui", NULL };
 static const Key keys[] = {
