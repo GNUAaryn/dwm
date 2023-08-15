6e118ebd5fe3e0078c3c6b69408ae517bf068907
GNUAaryn <90221167+GNUAaryn@users.noreply.github.com>
2023-08-15 06:02:59 -0500

    Update config.h (drac time!!)

--- config.h
+++ config.h
@@ -8,12 +8,12 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=15" };
 static const char dmenufont[]       = "monospace:size=15";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col_urgborder[]   = "#ff0000";
+static const char col_gray3[]       = "#f8f8f2";
+static const char col_gray4[]       = "#f8f8f2";
+static const char col_cyan[]        = "#bd93f9";
+static const char col_urgborder[]   = "#bd93f9";
 
 //#include "/home/aaron/.cache/wal/colors-wal-dwm.h"
 static const char *colors[][3]      = {
